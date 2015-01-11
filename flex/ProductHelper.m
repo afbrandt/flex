296d4c07eb8742f675ae2259d86f9bfc3521055b
Andrew Brandt <abrandt@makeschool.com>
2015-01-11 15:51:36 -0800

    item scan makes http request for information

--- flex/ProductHelper.m
+++ flex/ProductHelper.m
@@ -11,15 +11,24 @@
 #import "AppDelegate.h"
 #import "FlexProduct.h"
 
+@interface ProductHelper ()
+
+@property (nonatomic, strong) AFHTTPRequestOperationManager *manager;
+
+@end
+
 @implementation ProductHelper
 
 static ProductHelper *helper;
+
+static NSString const* URL_BASE = @"http://api.upcdatabase.org/json";
 
 - (instancetype)init {
     self = [super init];
     
     if (self) {
         self.context = ((AppDelegate *)[[UIApplication sharedApplication] delegate]).managedObjectContext;
+        self.manager = [AFHTTPRequestOperationManager manager];
     }
     
     return self;
@@ -28,6 +37,14 @@ static ProductHelper *helper;
 - (void)processUPC:(NSString *)upcString {
     FlexProduct *product = [FlexProduct createInstanceFromManagedContext:self.context];
     NSError *error;
+    
+    NSString *url = [NSString stringWithFormat:@"%@/e211badfdf61541535d3e7c4e8d4d2f6/%@", URL_BASE, upcString];
+    
+    [self.manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"json: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"error");
+    }];
     
     [product setUpc:upcString];
     [self.context save:&error];
